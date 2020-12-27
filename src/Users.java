e16065f3f7724659fa32de86553d150f317853b5
LGBBR <1828901325@qq.com>
2020-12-27 13:35:47 +0800

    添加表格

---
+++ src/Users.java
@@ -0,0 +1,38 @@
+public class Users {
+    private String userName;
+    private String passWord;
+    private String address;
+    private  String phone;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
