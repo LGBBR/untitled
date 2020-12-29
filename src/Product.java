dd6510a9b51418a8880a87eeb309c888701cafef
LGBBR <1828901325@qq.com>
2020-12-29 15:05:08 +0800

    添加Product.java、products.xls表格、ReadProduct.java

---
+++ src/Product.java
@@ -0,0 +1,38 @@
+public class Product {
+    private String id;
+    private String productName;
+    private float price;
+    private String description;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
